bf5197506e8e463401e2d0ffd33d9cc38e3a0be7
Lucas <lcseferreira@hotmail.com>
2022-10-23 23:08:43 -0300

    Strings

---
+++ Trilha Fundamentar/Guia Estelar JavaScript/script.js
@@ -0,0 +1,14 @@
+/*
+
+    String
+
+    "" // aspas duplas
+    '' // aspas siples
+    `` // template literals ou template strings ({interpolação})
+
+*/
+
+console.log('Lucas"Eduardo');
+console.log("Lucas'Eduardo");
+console.log(`Lucas Eduardo, ${12 + 12} anos!
+Quebra de linha com template strings...`);
