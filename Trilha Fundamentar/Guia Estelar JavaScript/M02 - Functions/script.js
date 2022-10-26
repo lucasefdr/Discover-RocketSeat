6d30b876b3f2af640094db4eb92c301e78218814
Lucas <lcseferreira@hotmail.com>
2022-10-26 17:06:34 -0300

    Functions - Introduction

---
+++ Trilha Fundamentar/Guia Estelar JavaScript/M02 - Functions/script.js
@@ -0,0 +1,11 @@
+// app de frases motivacionais
+
+// declaration - function statement
+function createPhrases() {
+  console.log("Paciência e persistência");
+  console.log("Revisão é mãe do aprendizado");
+}
+
+// execute, run, call, invoke function
+createPhrases();
+createPhrases();
