18e1fba8980c518d650e98fa3b4c810de229de72
Lucas <lcseferreira@hotmail.com>
2022-10-27 10:35:08 -0300

    comparission operators

---
+++ Trilha Fundamentar/Guia Estelar JavaScript/M04-Expressions/arithmetic-operators.js
@@ -0,0 +1,16 @@
+// arithmetic operators
+let numberOne = 78;
+let numberTwo = 44;
+
+console.log("adição: " + (numberOne + numberTwo));
+console.log("subtração: " + (numberOne - numberTwo));
+console.log("multiplicação: " + numberOne * numberTwo);
+console.log("divisão: " + numberOne / numberTwo);
+console.log("módulo: " + (numberOne % numberTwo));
+console.log("incremento: " + ++numberOne);
+console.log("decremento: " + --numberOne);
+console.log("exponencial: " + numberOne ** 2);
+
+// grouping operator ()
+let total = (2 + 3) * 5;
+console.log(total);
---
+++ Trilha Fundamentar/Guia Estelar JavaScript/M04-Expressions/comparission-operators.js
@@ -0,0 +1,33 @@
+// comparission operators
+let one = 1;
+let two = 2;
+
+// == -> igual a
+console.log("==");
+console.log(one == 1);
+console.log(one == "1");
+console.log(one == 2);
+
+// === -> estritramente igual a
+console.log("===");
+console.log(one === "1");
+
+// != -> diferente de
+console.log("!=");
+console.log(one != 1);
+console.log(one != "1");
+console.log(one != 2);
+
+// === -> estritramente diferente de
+console.log("!==");
+console.log(one !== "1");
+
+// >= -> maior ou igual a
+console.log(">=");
+console.log(one >= 1);
+console.log(one >= 2);
+
+// <= -> menor ou igual a
+console.log("<=");
+console.log(one <= 1);
+console.log(one <= 2);
