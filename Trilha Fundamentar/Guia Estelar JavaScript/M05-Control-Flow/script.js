a591b8cbb80e3c4dc3551f88d132e7bbb33a4fc7
Lucas <lcseferreira@hotmail.com>
2022-10-27 23:43:53 -0300

    Control Flow - switch

---
+++ Trilha Fundamentar/Guia Estelar JavaScript/M05-Control-Flow/script.js
@@ -0,0 +1,18 @@
+// switch
+let expression = "a";
+
+switch (expression) {
+  case "a":
+    console.log("a");
+    break;
+  case "b":
+    console.log("b");
+    break;
+  case "c":
+    console.log("c");
+    break;
+
+  default:
+    console.log("default");
+    break;
+}
