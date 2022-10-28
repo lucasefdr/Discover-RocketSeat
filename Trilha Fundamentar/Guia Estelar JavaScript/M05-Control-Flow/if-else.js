e406c4d637317eb340ad231aaf6c87c39cc9872f
Lucas <lcseferreira@hotmail.com>
2022-10-27 23:43:12 -0300

    Control Flow - if...else

---
+++ Trilha Fundamentar/Guia Estelar JavaScript/M05-Control-Flow/if-else.js
@@ -0,0 +1,10 @@
+// if...else
+let temperature = 37.6;
+
+if (temperature >= 37.5) {
+  console.log("Febre alta!");
+} else if (temperature >= 37) {
+  console.log("Febre!");
+} else {
+  console.log("Saudável!");
+}
