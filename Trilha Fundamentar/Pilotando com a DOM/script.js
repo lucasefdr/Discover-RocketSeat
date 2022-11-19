757ba368d6bee53361512d5b38862d6c09d7a734
Lucas <lucas.efdrosa@gmail.com>
2022-11-19 16:15:30 -0300

    DOM - getElementsByTagName

--- Trilha Fundamentar/Pilotando com a DOM/script.js
+++ Trilha Fundamentar/Pilotando com a DOM/script.js
@@ -2,6 +2,13 @@
 const element = document.getElementById("blog-title");
 console.log(element);
 
-// getElementsByClassName
+// getElementsByClassName -> pegando os elementos pela classe
 const elements = document.getElementsByClassName("class-name");
-console.log(elements);
+console.log(elements[0]);
+console.log(elements[1]);
+
+// getElementsByTagName -> pegando os elementos pela tag
+const elementsByTag = document.getElementsByTagName("meta");
+console.log(elementsByTag[0]);
+console.log(elementsByTag[1]);
+console.log(elementsByTag[2]);
