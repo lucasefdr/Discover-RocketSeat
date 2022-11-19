9403bcb3b57754d5afd248600777b41ce2ca2f91
Lucas <lucas.efdrosa@gmail.com>
2022-11-19 16:13:11 -0300

    DOM - getElementsByClassName

--- Trilha Fundamentar/Pilotando com a DOM/script.js
+++ Trilha Fundamentar/Pilotando com a DOM/script.js
@@ -1,3 +1,7 @@
 // getElementById() -> pegando o elemento HTML pelo ID
 const element = document.getElementById("blog-title");
 console.log(element);
+
+// getElementsByClassName
+const elements = document.getElementsByClassName("class-name");
+console.log(elements);
